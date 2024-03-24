93e755cc0283c59675a8bfe42c419c952800236d
at0x123 <vp.ca94@gmail.com>
2024-03-24 17:56:13 -0500

    scaffold frog base build

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -3,55 +3,31 @@ import { Metadata } from "next";
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
 import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
+import { getFrameMetadata } from 'frog/next'
 
 const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : `http://localhost:${process.env.PORT || 3000}`;
+    ? `https://${process.env.VERCEL_URL}`
+    : `http://localhost:${process.env.PORT || 3000}`;
 const imageUrl = `${baseUrl}/thumbnail.jpg`;
 
-export const metadata: Metadata = {
-  metadataBase: new URL(baseUrl),
-  title: {
-    default: "Scaffold-ETH 2 App",
-    template: "%s | Scaffold-ETH 2",
-  },
-  description: "Built with 🏗 Scaffold-ETH 2",
-  openGraph: {
-    title: {
-      default: "Scaffold-ETH 2 App",
-      template: "%s | Scaffold-ETH 2",
-    },
-    description: "Built with 🏗 Scaffold-ETH 2",
-    images: [
-      {
-        url: imageUrl,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    images: [imageUrl],
-    title: {
-      default: "Scaffold-ETH 2",
-      template: "%s | Scaffold-ETH 2",
-    },
-    description: "Built with 🏗 Scaffold-ETH 2",
-  },
-  icons: {
-    icon: [{ url: "/favicon.png", sizes: "32x32", type: "image/png" }],
-  },
-};
+export async function generateMetadata(): Promise<Metadata> {
+    const url = process.env.VERCEL_URL || 'http://localhost:3000'
+    const frameMetadata = await getFrameMetadata(`${url}/api`)
+    return {
+        other: frameMetadata,
+    }
+}
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <html suppressHydrationWarning>
-      <body>
-        <ThemeProvider enableSystem>
-          <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html suppressHydrationWarning>
+            <body>
+                <ThemeProvider enableSystem>
+                    <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 };
 
 export default ScaffoldEthApp;
